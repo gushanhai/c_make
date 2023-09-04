40406300a6bf7118fbcf7838cc6315c70c559167
gushanhai <2558700527@qq.com>
2023-09-04 23:15:48 +0800

    9.4力扣刷题和预处理常用宏

---
+++ 9.4.c
@@ -0,0 +1,89 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:4996)	
+//#include<stdio.h>
+//#define DEBUG_PRINTR2 printf("file:%s\t\
+//							  line:%d\t\
+//							  date:%s\ttime:%s\t\n",\
+//								__FILE__,__LINE__,__DATE__,__TIME__)
+//#define DEBUG_PRINTR printf("file:%s\nline:%d\ndate:%s\ttime:%s\t\n",__FILE__,__LINE__,__DATE__,__TIME__)
+//int main()
+//{
+//	DEBUG_PRINTR;
+//	printf("\n\n");
+//	DEBUG_PRINTR2;
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#define M 100;
+//int main()
+//{
+//	printf("%d", M);
+//	return 0;
+//}
+
+//给你两个二进制字符串 a 和 b ，以二进制字符串的形式返回它们的和。
+//void reserve(char* s) {
+//    int len = strlen(s);
+//    for (int i = 0; i < len / 2; i++) {
+//        char t = s[i];
+//        s[i] = s[len - i - 1], s[len - i - 1] = t;
+//    }
+//}
+//char* addBinary(char* a, char* b)
+//{
+//    reserve(a);//反转字符串a
+//    reserve(b);//反转字符串b
+//    int len1 = strlen(a);//计算a的长度
+//    int len2 = strlen(b);//计算b的长度
+//    int max = fmax(len1, len2);//fmax函数计算两个数中的较大值
+//    int len = 0;//返回数组的下标
+//    int carry = 0; //carry表示进位
+//    char* ret = (char*)malloc(1 * (max + 2));//动态开辟！*（max+2）的空间
+//    for (int i = 0; i < max; i++)
+//    {
+//        carry += i < len1 ? (a[i] == '1') : 0;//
+//        carry += i < len2 ? (b[i] == '1') : 0;
+//        ret[len++] = carry % 2 + '0';
+//        carry /= 2;
+//    }
+//    if (carry)
+//    {
+//        ret[len++] = '1';
+//    }
+//    ret[len] = '\0';
+//    reserve(ret);
+//    return ret;
+//}
+
+//给你一个非负整数 x ，计算并返回 x 的 算术平方根 。
+//方法1：
+//int mySqrt(int x)
+//{
+//    long long i = 1;
+//    while (i * i <= x)
+//    {
+//        i++;
+//    }
+//    return i - 1;
+//}
+
+//方法2：
+//int mySqrt(int x)
+//{
+//    if (x == 1)
+//        return x;
+//    long long left = 1;
+//    long long right = x / 2;
+//    while (left <= right)
+//    {
+//        long long mid = (left + right) / 2;
+//        if (mid * mid < x)
+//            left = mid + 1;
+//        else if (mid * mid > x)
+//            right = mid - 1;
+//        else
+//            return mid;
+//    }
+//    return left - 1;
+//}
