d9ed883ca72401d4eedba7c4c85791661759e651
gushanhai <2558700527@qq.com>
2023-09-26 09:23:46 +0800

    9.26直接选择排序、冒泡排序、快排、希尔排序

--- sort.c
+++ sort.c
@@ -1,4 +1,12 @@
 #include"sort.h"
+
+void Swap(int* a1, int* a2)
+{
+	int tmp = *a1;
+	*a1 = *a2;
+	*a2 = tmp;
+}
+
 void InsertSort(int* arr, int n)
 {
 	for (int i = 0; i < n - 1; i++)
@@ -7,11 +15,13 @@ void InsertSort(int* arr, int n)
 		int tmp = arr[end + 1];
 		while (end >= 0)
 		{
-			if (tmp < arr[end])
+			if (arr[end] > tmp)
+			{
 				arr[end + 1] = arr[end];
+				end--;
+			}
 			else
 				break;
-			end--;
 		}
 		arr[end + 1] = tmp;
 	}
@@ -22,14 +32,14 @@ void ShellSort(int* arr, int n)
 	int gap = n;
 	while (gap > 1)
 	{
-		gap /= 2;
-		for (int j = 0; j < n - gap; j += gap)
+		gap = gap / 3 + 1;
+		for (int i = 0; i < n - gap; i++)
 		{
-			int end = j;
+			int end = i;
 			int tmp = arr[end + gap];
 			while (end >= 0)
 			{
-				if (tmp < arr[end])
+				if (arr[end] > tmp)
 				{
 					arr[end + gap] = arr[end];
 					end -= gap;
@@ -41,3 +51,172 @@ void ShellSort(int* arr, int n)
 		}
 	}
 }
+
+void PrintArry(int* arr, int n)
+{
+	assert(arr);
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+
+void BubbleSort(int* arr, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		int flag = 0;
+		for (int j = 0; j < n - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				Swap(&arr[j], &arr[j + 1]);
+				flag = 1;
+			}
+		}
+		if (!flag)
+		{
+			break;
+		}
+	}
+}
+
+void SelectSort(int* arr, int n)//选择排序
+{
+	int start = 0;
+	int end = n - 1;
+	while(start<end)
+	{
+		int max = start;
+		int min = start;
+		for (int i = start+1; i <=end; i++)
+		{
+			if (arr[i] > arr[max])
+			{
+				max = i;
+			}
+			if (arr[i] < arr[min])
+			{
+				min = i;
+			}
+		}
+		Swap(&arr[start], &arr[min]);
+		if (max == start)
+			max = min;
+		Swap(&arr[max], &arr[end]);
+		end--;
+		start++;
+	}
+}
+
+int GetMidi(int* a, int left, int right)//三数取中 
+{
+	int mid = (left + right) / 2;
+	// left mid right
+	if (a[left] < a[mid])
+	{
+		if (a[mid] < a[right])
+		{
+			return mid;
+		}
+		else if (a[left] > a[right])  // mid是最大值
+		{
+			return left;
+		}
+		else
+		{
+			return right;
+		}
+	}
+	else // a[left] > a[mid]
+	{
+		if (a[mid] > a[right])
+		{
+			return mid;
+		}
+		else if (a[left] < a[right]) // mid是最小
+		{
+			return left;
+		}
+		else
+		{
+			return right;
+		}
+	}
+}
+
+int PartSort(int* arr,int left,int right)//hoare法：
+{
+	int mid = GetMidi(arr, left, right);
+	Swap(&arr[mid], &arr[left]);
+
+	int key = left;
+	while (left < right)
+	{
+		while (left < right && arr[right] >= arr[key])
+			right--;
+		while (left < right && arr[left] <= arr[key])
+			left++;
+		Swap(&arr[left], &arr[right]);
+	}
+	Swap(&arr[left], &arr[key]);
+	return left;
+}
+
+int PartSort2(int* arr, int left, int right)//蹲坑法:
+{
+	int mid = GetMidi(arr, left, right);
+	Swap(&arr[left], &arr[mid]);
+
+	int key = arr[left];
+	int kenwei = left;
+	while (left < right)
+	{
+		while (left<right && arr[right] >= key)
+		{
+			right--;
+		}
+		arr[kenwei] = arr[right];
+		kenwei = right;
+		while (left < right && arr[left] <= key)
+		{
+			left++;
+		}
+		arr[kenwei] = arr[left];
+		kenwei = left;
+	}
+	arr[kenwei] = key;
+	return left;
+}
+
+int PartSort3(int* arr, int left, int right)//前后指针法：
+{
+	int mid = GetMidi(arr, left, right);
+	Swap(&arr[left], &arr[mid]);
+
+	int per = left;
+	int cur = left + 1;
+	int key = left;
+	while (cur <=right)
+	{
+		if (arr[cur] < arr[key]&&++per!=cur)
+		{
+			Swap(&arr[cur], &arr[per]);
+		}
+		++cur;
+	}
+	Swap(&arr[key], &arr[per]);
+	return per;
+}
+
+void QuickSort(int* arr, int start, int end)//快排
+{
+	if (start >= end)
+	{
+		return;
+	}
+	int key = PartSort(arr, start, end);
+	QuickSort(arr, start, key - 1);	
+	QuickSort(arr, key + 1, end);
+} 
--- sort.h
+++ sort.h
@@ -1,7 +1,17 @@
 #pragma once
 #include<stdio.h>
-
+#include<assert.h>
+#include<stdlib.h>
+#include<time.h>
 
 void InsertSort(int* arr,int n);
 
 void ShellSort(int* arr, int n);
+
+void PrintArry(int* arr, int n);
+
+void BubbleSort(int* arr, int n);
+
+void SelectSort(int* arr, int n);
+
+void QuickSort(int* arr, int start, int end);
